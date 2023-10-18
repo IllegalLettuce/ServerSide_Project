975e2701b3e80e88cf64526c128d1b6f8d6ba6d6
Anthony Sabbe <anthonysabbe@gmail.com>
2023-10-18 13:09:16 +0100

    Connect
    Connected to MongoDB
    BUT CAN I CHANGE ANYTHING OOOOOOOO

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const mongoose = require('mongoose');  //mongoose
+var mongoose = require('mongoose');  //mongoose
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
